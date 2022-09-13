106fd074225a009ee88352c8b5863a8f0e7ba4df
Aimee <aimee_vu@outlook.com>
2022-09-12 21:00:55 -0400

    Completed script to scrape data, completed flask app.py to load data into MongoDB and display it

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scrape_mars
+import json
+from bson import json_util
+
+app = Flask(__name__)
+
+# Databasae Connection with PyMongo
+app.config["MONGO_URI"] = "mongodb://localhost:27017/marsDataDB"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    marsData = mongo.db.marsData.find_one()
+    return json.loads(json_util.dumps(marsData))
+
+@app.route("/scrape")
+def scrape():
+    # Databse Reference
+    marsTable = mongo.db.marsData
+
+    # Drops table if it exists
+    mongo.db.marsData.drop()
+
+    # Calls script to scrap data
+    marsData = scrape_mars.scrape_all()
+
+    # Loads dictionary into MongoDB
+    marsTable.insert_one(marsData)
+
+    # Returns data
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run()
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,20 +1,25 @@
 # Imports
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
+import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape_all():
     #print("Scrape all was reached")
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome',**executable_path, headless=False)
+    path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome',**path, headless=False)
 
     # Get info from news page
-    news_title, news_paragraph = scrape_news(browser)
+    newsTitle, newsParagraph = scrapeNews(browser)
 
-    # Build dictionary from scraped information
+    # Dictionary of scraped information
     marsData = {
-        "newsTitle": news_title,
-        "newsParagraph": news_paragraph
+        "newsTitle": newsTitle,
+        "newsParagraph": newsParagraph,
+        "featuredImage": scrapeFeatureImg(browser),
+        "facts": scrapeFactsPage(browser),
+        "hemispheres": scrapeHemispheres(browser),
+        "lastUpdated": dt.datetime.now()
     }
 
     # Stops webdriver
@@ -22,7 +27,8 @@ def scrape_all():
 
     return marsData
 
-def scrape_news(browser):
+# Scrapes news pages
+def scrapeNews(browser):
     # Visit news site
     # With the wait_time
     url = 'https://redplanetscience.com/'
@@ -32,17 +38,92 @@ def scrape_news(browser):
 
     # Convert browser html to soup object
     html = browser.html
-    news_soup = soup(html, 'html.parser')
+    newsSoup = soup(html, 'html.parser')
 
     # Display current title content
-    slide_elem = news_soup.select_one('div.list_text')
-    news_title = slide_elem.find('div', class_='content_title').get_text()
+    slideElem = newsSoup.select_one('div.list_text')
+    newsTitle = slideElem.find('div', class_='content_title').get_text()
 
     # Uses parent element to find paragraph text
-    news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    newsP = slideElem.find('div', class_='article_teaser_body').get_text()
 
     # Returns title and paragraph
-    return news_title, news_p
+    return newsTitle, newsP
 
+# Scrapes for Feature Image
+def scrapeFeatureImg(browser):
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+
+    # Find and click the full image button
+    fullImageLink = browser.find_by_tag('button')[1]
+    fullImageLink.click()
+
+    # Parse html with soup
+    html = browser.html
+    imgSoup = soup(html, 'html.parser')
+
+    # Find relative image url
+    imgUrlRel = imgSoup.find('img', class_='fancybox-image').get('src')
+
+    # Store image url as absolute url to use later
+    imgUrl = f'https://spaceimages-mars.com/{imgUrlRel}'
+
+    # Return image URL
+    return imgUrl
+
+# Scrape Facts Page
+def scrapeFactsPage(browser):
+    # Visit URL
+    url = 'https://galaxyfacts-mars.com/'
+    browser.visit(url)
+
+    # Parse through HTML with soup
+    html = browser.html
+    factsSoup = soup(html, 'html.parser')
+
+    # Find facts location
+    factsLocation = factsSoup.find('div', class_="diagram mt-4")
+    factsTable = factsLocation.find('table') # Grabs table html code
+
+    # Empty string
+    facts = ""
+
+    # Adds text to empty string
+    facts += str(factsTable)
+
+    return facts
+
+# Scrap Hemispheres Page
+def scrapeHemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # List to hold images and titles
+    hemisphereImgUrl = []
+
+    for i in range(4):
+        hemisphereInfo = {}
+    
+        # Find element on each loop
+        browser.find_by_css('a.product-item img')[i].click()
+        
+        # Find sample image anchor tag and extract href
+        sample = browser.links.find_by_text('Sample').first
+        hemisphereInfo['img_url'] = sample['href']
+        
+        # Get title
+        hemisphereInfo['title'] = browser.find_by_css('h2.title').text
+        
+        # Append object to list
+        hemisphereImgUrl.append(hemisphereInfo)
+        
+        # Navigate back
+        browser.back()
+    
+    return hemisphereImgUrl
+
+# Sets up flask app
 if __name__ == "__main__":
     print(scrape_all())
